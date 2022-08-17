1872dfa603bd997da0cab187a90da146b17c3ce5
SallyMulupi <mulupisally2gmail.com>
2022-08-17 22:32:04 +0300

    commit tree fbf616962c6f729dd3e455728ec96ceadca57d4a

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,69 +1,65 @@
-require_relative 'spec_helper'
-require_relative '../enumerables'
+require 'pry'
 
-describe '#get_names' do
-
-  it 'returns an array of names' do
-    expect(get_names(spicy_foods)).to eq(["Green Curry", "Buffalo Wings", "Mapo Tofu"])
-  end
-
+# this method returns an array of hashes, which we'll use in the other methods
+def spicy_foods
+  [
+    { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
+    { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
+    { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 },
+  ]
 end
 
-describe '#spiciest_foods' do
-  
-  it 'return an array of hashes where the heat level of the food is greater than 5' do
-    expect(spiciest_foods(spicy_foods)).to eq([
-      { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
-      { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 } 
-    ])
-  end
+#takes an array of spicy_foods and returns an array of strings with the names of each spicy food.
+def get_names(spicy_foods)
+  food_name =spicy_food.map{|b| b[:name]}
+  return food_name
+end
+# array of spicy_foods and returns an array of hashes where the heat level of the food is greater than 5.
 
+def spiciest_food(picy_foods)
+  level = spicy_foods.select{|b| b[:heat_level] >= 5}
+
+  return level
 end
 
-describe '#print_spicy_foods' do
-  
-  it 'outputs the spicy foods to the terminal in the specified format' do
-    output_str = "Green Curry (Thai) | Heat Level: 🌶🌶🌶🌶🌶🌶🌶🌶🌶\nBuffalo Wings (American) | Heat Level: 🌶🌶🌶\nMapo Tofu (Sichuan) | Heat Level: 🌶🌶🌶🌶🌶🌶\n"
-    expect { print_spicy_foods(spicy_foods) }.to output(output_str).to_stdout
-  end
-  
+
+#takes an array of spicy_foods and output to the terminal in the format #puts:Buffallo wings (American)|heat level
+def print_spicy_foods(spicy_foods)
+  return spicy_foods.each {|b| puts "#{b[:name]} (#{b[:cuisine]}) | Heat Level: #{'🌶' * b[:heat_level]}"}
 end
 
-describe '#get_spicy_food_by_cuisine' do
-  
-  it 'returns a single hash for the spicy food whose cuisine matches the cuisine being passed to the method' do
-    expect(get_spicy_food_by_cuisine(spicy_foods, "Sichuan")).to eq({ name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 })
-    expect(get_spicy_food_by_cuisine(spicy_foods, "Thai")).to eq({ name: 'Green Curry', cuisine: 'Thai', heat_level: 9 })
-  end
 
+
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine)
+  selected = spicy_foods.each{|b| puts 
+  if b[:cuisine] == cuisine
+  return b
+end
+}
+end
+ get_spicy_food_by_cuisine(spicy_foods, "American")
+
+
+
+
+ def sort_by_heat(spicy_foods)
+  sorted = spicy_foods.sort{|b1, b2| b1[:heat_level] <=> b2[:heat_level]}
+  return sorted
 end
 
-describe '#sort_by_heat' do
-  
-  it 'return an array of hashes sorted by heat level from lowest to highest' do
-    sorted_array = [
-      { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
-      { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 },
-      { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 }
-    ]
-    expect(sort_by_heat(spicy_foods)).to eq(sorted_array)
-  end
 
+
+def print_spiciest_foods(spicy_foods)
+  levels = spicy_foods.filter{|b| b[:heat_level] > 5 }
+  return levels.each {|b| puts "#{b[:name]} (#{b[:cuisine]}) | Heat Level: #{'🌶' * b[:heat_level]}"}
 end
 
-describe '#print_spiciest_foods' do
-  
-  it 'outputs ONLY the spicy foods that have a heat level greater than 5 to the terminal in the specified format' do
-    output_str = "Green Curry (Thai) | Heat Level: 🌶🌶🌶🌶🌶🌶🌶🌶🌶\nMapo Tofu (Sichuan) | Heat Level: 🌶🌶🌶🌶🌶🌶\n"
-    expect { print_spiciest_foods(spicy_foods) }.to output(output_str).to_stdout
-  end
-  
-end
 
-describe '#average_heat_level' do
-  
-  it 'returns an integer representing the average heat level of all the spicy foods in the array' do
-    expect(average_heat_level(spicy_foods)).to eq(6)
-  end
-  
+
+
+def average_heat_level(spicy_foods)
+  total_level = spicy_foods.sum{|b| b[:heat_level]}
+
+  return total_level/spicy_foods.size
 end
